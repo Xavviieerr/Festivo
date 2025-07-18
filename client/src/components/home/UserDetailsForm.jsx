96075ce6e3b2cd42b5da76edafcf0a4beda6a881
Xavviieerr <ogbupaul545@gmail.com>
2025-07-18 22:16:21 +0100

    working on the profile completeion form

---
+++ client/src/components/home/UserDetailsForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const UserDetailsForm = () => {
+  return (
+    <>
+      <div
+        className="fixed inset-0 z-[999] bg-black opacity-50
+      flex justify-center items-center
+      "
+      >
+        <div className="h-1/2 w-1/2 ring bg-white flex">
+          <div className="w-1/2 border h-auto border-amber-600">a</div>
+          <div className="w-1/2 h-auto">b</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserDetailsForm;
--- client/src/pages/home/Dashboard.jsx
+++ client/src/pages/home/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import UserDetailsForm from "../../components/home/userDetailsForm";
+const isProfileComplete = true;
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="h1">Dashboard</div>
+      {isProfileComplete && (
+        <>
+          <h1>Inner</h1>
+          <UserDetailsForm />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
