a0f5955a1c62f8c3a0ff85f568ab59adc283df8b
Xavviieerr <ogbupaul545@gmail.com>
2025-08-22 13:45:15 +0100

    implemented the condition for the profile form to appear if the user profile is not complete and vice versa

--- client/src/pages/home/Dashboard.jsx
+++ client/src/pages/home/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import UserDetailsForm from "../../components/home/userDetailsForm";
-const isProfileComplete = true;
+import UserDetailsForm from "../../components/home/UserDetailsForm";
+import { useSelector } from "react-redux";
 const Dashboard = () => {
+  const isProfileComplete = useSelector(
+    (state) => state.authReducer.authData?.user.isProfileComplete
+  );
   return (
     <>
       <div className="h1">Dashboard</div>
-      {isProfileComplete && (
+
+      {/* Form for requesting extra data from the user if their profile is not complete */}
+      {!isProfileComplete && (
         <>
-          <h1>Inner</h1>
           <UserDetailsForm />
         </>
       )}
