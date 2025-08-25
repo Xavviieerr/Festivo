9a4623405e45ee0f78d799c8f01f8385aca78432
Xavviieerr <ogbupaul545@gmail.com>
2025-08-25 17:44:56 +0100

    completed profile page with dynamic data

--- client/src/components/home/DashboardTopBar.jsx
+++ client/src/components/home/DashboardTopBar.jsx
@@ -10,14 +10,6 @@ const DashboardTopBar = () => {
     <div className="flex justify-between items-center px-10 py-2 shadow-[0_4px_4px_-2px_rgba(0,0,0,0.3)]">
       <div className="font-bold text-xl text-concrete-700">Dashboard</div>
       <div className="flex items-center gap-3">
-        <span className="h-12.5 w-12.5 rounded-full bg-gray-900 text-white flex justify-center items-center">
-          <img
-            src={Logo}
-            className="rounded-full object-cover h-12 w-12"
-            alt="profile picture"
-          />
-        </span>
-
         <div className="flex flex-col text-sm ">
           <span className="text-gray-800 leading-none text-lg font-bold">
             {data.firstname && data.lastname
@@ -26,10 +18,17 @@ const DashboardTopBar = () => {
                 )}`
               : "John Doe"}
           </span>
-          <span className="text-xs leading-none text-gray-600">
+          <span className="text-xs flex flex-row-reverse text-gray-600">
             {data.admin ? "Admin" : "User"}
           </span>
         </div>
+        <span className="h-12.5 w-12.5 rounded-full bg-gray-900 text-white flex justify-center items-center">
+          <img
+            src={Logo}
+            className="rounded-full object-cover h-12 w-12"
+            alt="profile picture"
+          />
+        </span>
       </div>
     </div>
   );
--- client/src/components/home/Profile.jsx
+++ client/src/components/home/Profile.jsx
@@ -1,7 +1,95 @@
 import React from "react";
+import Friends from "../../assets/friends-with-pizza-drinks-low-angle.jpg";
+import Logo from "../../assets/logo2.png";
+import { capitalizeFirst } from "../../utils/capitalizeFirst";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const user = useSelector((state) => state.authReducer.authData.user);
+
+  return (
+    <div className="p-9">
+      <div className="profile h-96 flex flex-col gap-5">
+        <div className=" font-bold text-2xl text-gray-500">My Profile</div>
+        <div
+          className="topInfo shadow-[0px_3px_4px_1px_rgba(0,0,0,0.4)] mx-20
+         flex flex-col rounded justify-center"
+        >
+          <img
+            src={Friends}
+            alt=""
+            className="object-cover h-[190px] rounded-b-4xl "
+          />
+          <div className="p-3 rounded-full absolute top-[42%] left-[54%] bg-white">
+            <img src={Logo} alt="" className="h-30 w-30  rounded-full" />
+          </div>
+          <div
+            className="otherinfo mt-20 h-50 m-4 rounded-lg border border-gray-400
+          p-4 z-[1000] bg-white
+          "
+          >
+            <span className="font-bold text-gray-800 text-2xl">
+              Personal information
+            </span>
+            <div className="flex items-center justify-between px-20 mt-2 py-5">
+              <div>
+                <ul className="list-disc text-gray-700">
+                  <li>
+                    <span className="font-bold">Firstname: </span>{" "}
+                    <span className="text-sm">
+                      {user.firstname
+                        ? `${capitalizeFirst(user.firstname)}`
+                        : "John"}
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Lastname: </span>
+                    <span className="text-sm">
+                      {user.lastname
+                        ? `${capitalizeFirst(user.lastname)}`
+                        : "Doe"}
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Email: </span>
+                    <span className="text-sm">
+                      {user.email
+                        ? `${capitalizeFirst(user.email)}`
+                        : "example@gmail.com"}
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul className="list-disc text-gray-700">
+                  <li>
+                    <span className="font-bold">Gender: </span>{" "}
+                    <span className="text-sm">
+                      {user.gender ? `${capitalizeFirst(user.gender)}` : "None"}
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Status: </span>
+                    <span className="text-sm">
+                      {user.isAdmin ? "Admin" : "User"}
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">nationality: </span>
+                    <span className="text-sm">
+                      {user.nationality
+                        ? `${capitalizeFirst(user.nationality)}`
+                        : "None"}
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
